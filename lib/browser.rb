b85d2861e6b0709361aa7265a6cf9a1eba4c2607
nikklein <objcoding@gmail.com>
2016-12-07 22:13:45 +0000

    add browser class

---
+++ lib/browser.rb
@@ -0,0 +1,5 @@
+class Browser
+  def start
+    puts 'Hey buddy! It is a good time to browse some cool stuff'
+  end
+end
--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -2,8 +2,8 @@ describe 'Browser' do
   describe '#start' do
     it 'should output a greeting message' do
       browser = Browser.new
-      message = 'Hey buddy! It is a good time to visit some cool stuff'
-      expect(browser.start).to output(message).to_stdout
+      message = "Hey buddy! It is a good time to browse some cool stuff\n"
+      expect { browser.start }.to output(message).to_stdout
     end
   end
 end

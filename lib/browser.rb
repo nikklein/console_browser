352d5e9d21d8d5640089503c90f43f74c60a90bd
nikklein <objcoding@gmail.com>
2016-12-07 22:44:03 +0000

    add test output websites to visit

--- lib/browser.rb
+++ lib/browser.rb
@@ -1,5 +1,6 @@
 class Browser
   def start
-    puts 'Hey buddy! It is a good time to browse some cool stuff'
+    puts 'Hey buddy! It is a good time to browse some cool stuff.'
+    puts 'Choose any website from the list:'
   end
 end
--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -1,9 +1,14 @@
 describe 'Browser' do
   describe '#start' do
-    it 'should output a greeting message' do
+    it 'should output a greeting message to a command line' do
       browser = Browser.new
-      message = "Hey buddy! It is a good time to browse some cool stuff\n"
+      message = "Hey buddy! It is a good time to browse some cool stuff.\nChoose any website from the list:\n"
       expect { browser.start }.to output(message).to_stdout
+    end
+    it 'should output a choice of websites to visit' do
+      browser = Browser.new
+      options = 'moviepilot.com champions.co nowloading.co'
+      expect { browser.start }.to output(options).to_stdout
     end
   end
 end

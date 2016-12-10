81fcec25ff630298da18bce19d1f49c93cc3660d
nikklein <objcoding@gmail.com>
2016-12-10 13:41:33 +0000

    add test map_links

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,3 +1,4 @@
+require './MPsite/lib/card_title_tag_map_test.rb'
 
 describe 'Scraper' do
   before do
@@ -21,6 +22,17 @@ describe 'Scraper' do
       parsed_page = scraper.parse_page(link, xpath)
 
       expect(parsed_page.to_html).to eq(test_data)
+    end
+  end
+  describe '#map_links' do
+    it 'should map description and links from parsed data' do
+      scraper = Scraper.new
+      link = 'http://moviepilot.com'
+      xpath = '//h3[@class="card__title"]/a'
+      parsed_page = scraper.parse_page(link, xpath)
+      mapped_data = scraper.map_links(parsed_page)
+
+      expect(mapped_data).to eq(test_mapped_data)
     end
   end
 end
